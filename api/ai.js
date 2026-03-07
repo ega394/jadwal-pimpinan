279820d773cdfea318008257119925eb9f2d95a9
ega394 <ega394@users.noreply.github.com>
2026-03-07 11:18:37 +0800

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,4 +1,4 @@
-const https = require("https");
+import https from "https";
 
 function httpsPost(url, body) {
   return new Promise((resolve, reject) => {
@@ -39,7 +39,7 @@ function readBody(req) {
   });
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
